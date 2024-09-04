5a7d17221dcb25226659fe2c30e45dac3423f0d6
denis-wilton <deniswiltonpa@gmail.com>
2024-09-04 18:15:42 -0400

    Added router

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="h-full bg-black text-white grid place-content-center">
+        Blitz Volleyball!
+      </div>
+    </>
+  );
+}
